9da148cc936b24934b1602ec5ac248fc4c01b078
Kaven Vohra <vohrakaven@gmail.com>
2022-03-02 00:08:18 -0500

    added client services

--- src/app/clients/client-search-page/client-search-page.component.ts
+++ src/app/clients/client-search-page/client-search-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-client-search-page',
   templateUrl: './client-search-page.component.html',
-  styleUrls: ['./client-search-page.component.scss']
+  styleUrls: ['./client-search-page.component.scss'],
 })
 export class ClientSearchPageComponent implements OnInit {
+  clientList: any;
 
-  constructor() { }
+  constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
+    this.loadClientList();
   }
 
+  searchEventHandler(searchTerm): void {
+    if (searchTerm) {
+      this.searchClients(searchTerm);
+    } else {
+      this.loadClientList();
+    }
+  }
+
+  /*
+   * clientService.getClients() returns an observable that we subscribe to
+   * we then set clientList to the result of the call
+   */
+  private loadClientList(): void {
+    this.clientService.getClients().subscribe((result) => {
+      this.clientList = result;
+    });
+  }
+
+  private searchClients(searchTerm): void {
+    this.clientService.search(searchTerm).subscribe((res) => {
+      this.clientList = res;
+    });
+  }
 }

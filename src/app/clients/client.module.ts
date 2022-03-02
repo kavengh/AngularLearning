76ef876900c1a4b7b3cf72900b53489f779882e4
Kaven Vohra <vohrakaven@gmail.com>
2022-03-01 19:13:00 -0500

    imported components and updates routes

--- src/app/clients/client.module.ts
+++ src/app/clients/client.module.ts
@@ -1,31 +1,57 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ClientFormComponent } from './client-form/client-form.component';
-import { ClientPageComponent } from './client-page/client-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '../shared/shared.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { CustomMaterialModule } from '../custom-material.module';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { SharedModule } from '../shared/shared.module';
 import { ClientDetailComponent } from './client-detail/client-detail.component';
 import { ClientDetailsPageComponent } from './client-details-page/client-details-page.component';
 import { ClientEditPageComponent } from './client-edit-page/client-edit-page.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { ClientItemComponent } from './client-list/client-item/client-item.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientPageComponent } from './client-page/client-page.component';
 import { ClientSearchPageComponent } from './client-search-page/client-search-page.component';
+import { ClientService } from './client.service';
 
-/**
- * Here we will house all of our client related components and this will then allow us to
- * export this entire module into out app component. Also note that we have SharedModule as
- * an import which will allow us to get access to other components, services, etc we might need.
- */
-
+const routes: Routes = [
+  {
+    path: 'clients/new',
+    component: ClientPageComponent,
+  },
+  {
+    path: 'clients/search',
+    component: ClientSearchPageComponent,
+  },
+  {
+    path: 'clients/details/:id',
+    component: ClientDetailsPageComponent,
+  },
+  {
+    path: 'clients/edit/:id',
+    component: ClientEditPageComponent,
+  },
+];
 @NgModule({
-  // now we can add all of the components we made
-  declarations: [ClientFormComponent, ClientPageComponent, ClientDetailComponent, ClientDetailsPageComponent, ClientEditPageComponent, ClientSearchPageComponent],
-  // and since we want to use that sharedformcomp here we need to import the sharedmodule to get access
+  declarations: [
+    ClientPageComponent,
+    ClientSearchPageComponent,
+    ClientFormComponent,
+    ClientListComponent,
+    ClientItemComponent,
+    ClientDetailComponent,
+    ClientDetailsPageComponent,
+    ClientEditPageComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     SharedModule,
     CustomMaterialModule,
+    FormsModule,
+    RouterModule.forChild(routes),
   ],
+  providers: [ClientService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ClientModule {}

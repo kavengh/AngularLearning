a06393da6b49a7482e1a3d564e75547b6698158c
Kaven Vohra <vohrakaven@gmail.com>
2022-03-01 22:43:35 -0500

    company list item init

---
+++ src/app/company/company-list/company-list-item/company-list-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompanyListItemComponent } from './company-list-item.component';
+
+describe('CompanyListItemComponent', () => {
+  let component: CompanyListItemComponent;
+  let fixture: ComponentFixture<CompanyListItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompanyListItemComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompanyListItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

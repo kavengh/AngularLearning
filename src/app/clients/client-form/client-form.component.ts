9ea88bb7b7b8c13c8cefd44a0776022f6df8c0cf
Kaven Vohra <vohrakaven@gmail.com>
2022-03-01 22:59:35 -0500

    added save client event

--- src/app/clients/client-form/client-form.component.ts
+++ src/app/clients/client-form/client-form.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
-  Form,
   FormBuilder,
-  FormGroup,
   FormControl,
+  FormGroup,
   Validators,
 } from '@angular/forms';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-client-form',
@@ -14,18 +14,25 @@ import {
 })
 export class ClientFormComponent implements OnInit {
   // new FormGroup is defined here
-  // i believe this is my form group object
   clientForm: FormGroup;
 
+  @Input()
+  client: Client;
+
+  @Input()
+  edit = false;
+
+  @Output()
+  saveClientEvent: EventEmitter<Client> = new EventEmitter();
+
   // creating new FormControls, with validation
-  // these look to be my form control objects
   firstname = new FormControl('', Validators.required);
   lastname = new FormControl('', Validators.required);
   email = new FormControl('', Validators.required);
   telephoneNumber = new FormControl('');
   companyName = new FormControl('', Validators.required);
 
-  // in the constructor we create a FormGroup and set the properties of
+  // in the constructor we create the a FormGroup and set the properties of
   // the formGroup to the FormControls then set it to be the clientForm we use
   // in the template.
   constructor(fb: FormBuilder) {
@@ -38,10 +45,26 @@ export class ClientFormComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.client) {
+      console.log(this.client);
+    }
+  }
 
   // event called when form is submitted, displaying the output of the form
   saveClient() {
-    console.log(this.clientForm);
+    this.saveClientEvent.emit(this.createClient(this.clientForm));
+  }
+
+  private createClient(form: FormGroup) {
+    const clientObject: Client = {
+      firstname: form.value.firstname,
+      lastname: form.value.lastname,
+      email: form.value.email,
+      telephoneNumber: form.value.telephoneNumber,
+      companyName: form.value.companyName,
+    };
+
+    return clientObject;
   }
 }

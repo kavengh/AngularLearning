4ae90a22f64b2fff6e68b2034ae9ec85346185dd
Kaven Vohra <vohrakaven@gmail.com>
2022-03-02 22:21:48 -0500

    events to pass data to parent

--- src/app/clients/client-detail/client-detail.component.ts
+++ src/app/clients/client-detail/client-detail.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-client-detail',
   templateUrl: './client-detail.component.html',
-  styleUrls: ['./client-detail.component.scss']
+  styleUrls: ['./client-detail.component.scss'],
 })
-export class ClientDetailComponent implements OnInit {
+export class ClientDetailComponent {
+  editView = false;
 
-  constructor() { }
+  @Input()
+  client: Client;
 
-  ngOnInit(): void {
+  /**
+   * instatiation of the events, we do this so we can pass data to the parent component
+   */
+  @Output()
+  searchEvent: EventEmitter<Client> = new EventEmitter();
+
+  @Output()
+  editEvent: EventEmitter<Client> = new EventEmitter();
+
+  @Output()
+  deleteEvent: EventEmitter<Client> = new EventEmitter();
+
+  @Output()
+  saveEvent: EventEmitter<Client> = new EventEmitter();
+
+  /**
+   * methods that will pass the data to the parent
+   */
+
+  searchAll(): void {
+    this.searchEvent.emit();
   }
 
+  edit(): void {
+    this.toggleEdit();
+  }
+
+  delete(client: Client): void {
+    this.deleteEvent.emit(client);
+  }
+
+  save(): void {
+    this.saveEvent.emit(this.client);
+    this.toggleEdit();
+  }
+
+  private toggleEdit(): void {
+    this.editView = !this.editView;
+  }
 }
